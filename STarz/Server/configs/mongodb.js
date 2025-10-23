efcccd7a3990cd25dab788c85fb95c7c9ca8bd09
shakewithabhi <abhishek11singh2003@gmail.com>
2025-10-23 14:03:34 +0530

    testing

--- STarz/Server/configs/mongodb.js
+++ STarz/Server/configs/mongodb.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    //mongodb connection
     mongoose.connection.on("connected", () => console.log('database connected') );
-     
-    await mongoose.connect(`${process.env.MONGO_URI}/starz`, )
+   await mongoose.connect(`${process.env.MONGO_URI}/starz`, )
 }
 
 export default connectDB;
--- STarz/Server/server.js
+++ STarz/Server/server.js
@@ -4,15 +4,20 @@ import'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 
+//initialize express
 const app = express()
 
+//connect to database
 await connectDB();
 
+//middlewares
 app.use(cors())
 
 
+//routes
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/clerk',express.json(),clerkWebhooks)
 
+//start server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
