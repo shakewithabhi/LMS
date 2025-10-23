70ae8cef3233d8c3df1ebf7b28dafe99b2dc0f7a
shakewithabhi <abhishek11singh2003@gmail.com>
2025-10-23 14:21:53 +0530

    mongodburi

--- STarz/Server/configs/mongodb.js
+++ STarz/Server/configs/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     //mongodb connection
     mongoose.connection.on("connected", () => console.log('database connected') );
-   await mongoose.connect(`${process.env.MONGO_URI}/starz`, )
+   await mongoose.connect(`${process.env.MONGODB_URI}/starz`, )
 }
 
 export default connectDB;
